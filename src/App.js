37da0f21ceb8be5bb003fece048fde253fbf787f
Steven_Klavins <stevenklavins94@gmail.com>
2021-08-10 20:32:51 +0100

    chore: imported meals component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import {Fragment} from "react";
 import Header from "./Layout/Header";
+import Meals from "./Meals/Meals";
 function App() {
   return (
     <Fragment>
       <Header/>
+      <main>
+       <Meals/>
+      </main>
     </Fragment>
   );
 }
