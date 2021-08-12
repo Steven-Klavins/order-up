aa2e35d2ae376e683db0806a994d0530cbc3a0ba
Steven_Klavins <stevenklavins94@gmail.com>
2021-08-12 19:55:25 +0100

    chore: imported cart provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import {Fragment, useState} from "react";
+import {useState} from "react";
 import Header from "./Layout/Header";
 import Meals from "./Meals/Meals";
 import Cart from "./Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
 
@@ -16,13 +17,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler}/>}
       <Header onShowCart={showCartHandler}/>
       <main>
        <Meals/>
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
