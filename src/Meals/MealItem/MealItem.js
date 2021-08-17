5cd0147dbf763dcf270157aab1b9ef317a594c8a
Steven_Klavins <stevenklavins94@gmail.com>
2021-08-17 21:28:39 +0100

    chore: passed context in cart handler

--- src/Meals/MealItem/MealItem.js
+++ src/Meals/MealItem/MealItem.js
@@ -1,8 +1,25 @@
+import { useContext } from "react";
 import styles from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../store/cart-context";
 
 function MealItem(props) {
+
+  const cartCtx = useContext(CartContext);
+
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem(
+      {
+        id: props.id,
+        name: props.name,
+        amount: amount,
+        price: props.price
+      }
+    );
+  }
+
   return (
     <li className={styles.meal}>
       <div>
@@ -11,7 +28,7 @@ function MealItem(props) {
         <div className={styles.price}>{price}</div>
       </div>
       <div>
-          <MealItemForm id={props.id}/>
+          <MealItemForm onAddToCart={addToCartHandler} id={props.id}/>
       </div>
     </li>
   );
