35015081a50135b4ff59f87d51e5615589dd9394
Steven_Klavins <stevenklavins94@gmail.com>
2021-11-08 20:52:57 +0000

    feat: removed dummy data and added api call

--- src/Meals/AvailableMeals.js
+++ src/Meals/AvailableMeals.js
@@ -1,36 +1,52 @@
+import {useState, useEffect, useCallback} from "react";
 import styles from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 function AvailableMeals(props) {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMealsHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(process.env.REACT_APP_FIREBASE_URL);
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+      console.log(data)
+
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price
+
+        })
+      }
+
+      setMeals(loadedMeals);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMealsHandler();
+  }, [fetchMealsHandler]);
+
+
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
@@ -42,6 +58,8 @@ function AvailableMeals(props) {
   return (
     <section className={styles.meals}>
       <Card>
+        {isLoading && <Card><p>Loading meals....</p></Card>}
+        {error && <Card><p>Sorry something went wrong!</p></Card>}
         <ul>{mealsList}</ul>
       </Card>
     </section>
